dfd0b1c82db7355f56315afe847fbca37f0a6ac7
Pradeep Namdev Shinde <112394144+PradeepShinde24@users.noreply.github.com>
2025-05-10 07:00:12 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict";   // treat as JS code as newer version
+
+// alert (3+3)    // we are using nodejs, not browser
+
+console.log(3+3); console.log("Pradeep")     // not good practice
+
+
+console.log(3+3)
+console.log("Pradeep")
+
+
+let name = "Pradeep"
+let age= 18;
+let isLoogedIn = false;
+let state;
+
+  
+// number: 2 to power 53
+// bigint
+// string => " "
+// Boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log("typeof Keyword: ");
+
+console.log(typeof "pradeep");    // string
+console.log(typeof null);         // object
+console.log(typeof undefined);    // undefined
+
+
